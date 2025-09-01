4979db0cb0fa51d8f8052b65b394fbc8c5712c2f
debanjansinha979-tech <debanjansinha979@gmail.com>
2025-09-01 19:02:55 +0530

    ..Added paper component and also added icons and made alignment right according to the wireframe

--- src/Components/Requests/RequestPage.js
+++ src/Components/Requests/RequestPage.js
@@ -1,31 +1,38 @@
-import { Outlet } from 'react-router-dom';
-import MyTaskAddition from './MyTaskAddition'
-import RequestsTable from "./RequestsTable"
-import styled from 'styled-components'
+import { Outlet } from "react-router-dom";
+import MyTaskAddition from "./MyTaskAddition";
+import RequestsTable from "./RequestsTable";
+import styled from "styled-components";
+import { Paper } from "@mui/material";
 
 const ContentWrapper = styled.div`
   display: flex;
   justify-content: center;
-  align-items: center;
-  // margin-top:10px;
-  gap: 20px; /* only affects desktop horizontal gap */
+  align-items: flex-start;
+  gap: 20px;
 
   @media (max-width: 768px) {
     flex-direction: column;
-    flex-wrap:wrap;
-    align-items:center;
-    gap: 10; /* no vertical gap */
+    flex-wrap: wrap;
+    align-items: center;
+    gap: 10px;
   }
 `;
 
 export default function Request() {
   return (
-    <div>
+    <Paper
+      elevation={4}
+      sx={{
+        p: 3,
+        m: 1,
+        borderRadius: 3,
+      }}
+    >
       <ContentWrapper>
-        <MyTaskAddition/>
-        <RequestsTable/>
-        {/* <Outlet/> */}
+        <MyTaskAddition />
+        <RequestsTable />
+        {/* <Outlet /> */}
       </ContentWrapper>
-    </div>
-  )
+    </Paper>
+  );
 }
